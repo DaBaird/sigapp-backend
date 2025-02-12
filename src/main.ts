b74b4531d47c15c4aababc8f454e4c2a526ea6e8
DaBaird <terencejmccloskey@gmail.com>
2025-02-12 17:09:55 -0600

    Updated port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000; // ✅ Uses Azure's assigned port
+  const port = process.env.PORT || 8080; // ✅ Uses Azure's assigned port
   await app.listen(port);
 
   console.log(`Application running on port ${port}`);
